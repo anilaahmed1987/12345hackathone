1fbbad0c0c03767392b41f186a2862b09390728f
anilaahmed1987 <155479535+anilaahmed1987@users.noreply.github.com>
2024-11-12 17:25:35 +0500

    dynamic resume done

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ form.addEventListener("submit", function (event) {
     var experience = document.getElementById("experience").value;
     var skills = document.getElementById("skills").value;
     //generate resume content dynamically
-    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>email:</b>").concat(email, "</p>\n    <p><b>phone:</b>").concat(phone, "</p>\n\n    <h3> Education </h3>\n    <p>").concat(education, " </p>\n\n    <h3> Expeience </h3>\n    <p>").concat(experience, " </p>\n    \n    <h3> Skills </h3>\n    <p>").concat(skills, " </p>\n    \n    ");
+    var resumeHTML = "\n    <h2><b> Editable Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b><span contenditable=\"true\">".concat(name, "</span></p>\n    <p><b>email:</b><span contenditable=\"true\">").concat(email, "</span></p>\n    <p><b>phone:</b><span contenditable=\"true\">").concat(phone, "</span></p>\n\n    <h3> Education </h3>\n    <p> <contenditable=\"true\">").concat(education, " </p>\n\n    <h3> Expeience </h3>\n    <p> <contenditable=\"true\">").concat(experience, " </p>\n    \n    <h3> Skills </h3>\n    <p> <contenditable=\"true\">").concat(skills, " </p>\n    \n    ");
     //display the generated resume
     if (resumeDisplayElement) {
         resumeDisplayElement.innerHTML = resumeHTML;
